24530fe546ea6518eb65e6f70709cd7b551e1ebb
Abheek Satishkumar <147516145+abheekskumar@users.noreply.github.com>
2025-06-20 17:28:25 -0700

    Seperated files, worked on bisec, iter, lambda funcs

---
+++ Testing_Files/Algorithms_Methods/guess_approximate.py
@@ -0,0 +1,26 @@
+# guess and check or exhaustive enumeration:
+# you go through all possible values and guess each one.
+# time complexity of n(go through all possible sceanarios)
+# In this implementation, you could blow right across the number you're trying to find out. 
+# have measures in place for that too
+def basic_approximate_cube(n):
+    guess = 0
+    for i in range(n):
+        if guess**3 == n:
+            print(guess)
+        guess +=1
+
+def approximate_cube(n):
+    guess = 0
+    epsilon = 0.0001
+    increment = 0.1
+    numGuesses =0
+    while abs(guess**3-n) > epsilon:
+        if guess**3 == n:
+            print(guess)
+        guess +=increment
+        numGuesses +=1
+    print(guess,numGuesses)
+
+if __name__ == "__main__":
+    approximate_cube(27)
---
+++ Testing_Files/Algorithms_Methods/iteration.py
@@ -0,0 +1,41 @@
+"""
+Iterations are used to run the same blocks of code multiple times.
+There are 2 types of iterations:
+
+for loop - used when the no. of times of iterations is known
+while loop - used when the no. of times of iterations is unknown
+
+for loop:
+can be used to iterate through a string, list, tuple, and other data sequences
+no need to initialize a iterable var
+
+while loop:
+you have to initialize a iterable var
+you have to ensure that the conditions changes when you're in the loop, other wise you'll 
+run into an inf loop
+
+the jump statements:
+break - The break statement is used to exit the corresponding loop
+continue - The continue statements is used to move to the next iterable in the corresponding loop
+
+Other variations of the loops:
+do-while:
+The do while runs the code block atleast once and then checks the 
+"while" loop condition. 
+If it agrees, it continues, if it doesn't, it stops.
+
+Other Remarks:
+
+Both the loops have else clauses; the implementation is given down below
+The else clause is only run on normal flow of control; If there's a break, the else-clause is skipped
+"""
+
+
+l1 = list(range(20))
+x = 2
+for i in l1:
+    if i == x:
+        print("Object found at",l1.index(i))
+        break # if x found in 25, loop is exited unnaturally
+else:
+    print("Object not found") # if loop isn't existed naturally, else clause is skipped
