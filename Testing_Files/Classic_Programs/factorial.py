815e95e38cd0747e96c97efc333d39da2f3392e8
Abheek Satishkumar <147516145+abheekskumar@users.noreply.github.com>
2025-06-22 10:34:01 -0700

    Added Files

--- Testing_Files/Classic_Programs/factorial.py
+++ Testing_Files/Classic_Programs/factorial.py
@@ -11,9 +11,21 @@ def factorial_recur(num1):
     else:
         return None
     
+def factorial_iter(num2):
+    # factorial : n! = n*(n-1)*(n-2)...*1
+    if num2 == 0:
+        return 1
+    ans = 1 # var to keep track of ans
+    for i in range(1,num2+1): # list generated to make valid i values; for loop goes through each iteration
+        # to execute "updating ans var" constantly
+        ans = i*ans # updating ans var with i*new_ans
+    return ans
+
+
 if __name__ == "__main__":
     try:
         print(factorial_recur(int(input("Enter a number for its factorial:"))))
+        print(factorial_iter(int(input("Enter a number:"))))
     except Exception as e:
         pass
         print("The ",e,"has been thrown!")
--- Testing_Files/Classic_Programs/fibonacci.py
+++ Testing_Files/Classic_Programs/fibonacci.py
@@ -1,8 +1,30 @@
-def fibonacci(num1):
+count = 0
+def recur_fibonacci(num1):
+    # counter to reduce term output by 1.
+    global count
+    if count == 0:
+        num1 -=1
+    count +=1
+
+
     # base case: num1 == 1 or 0; return 1
     if num1 == 1 or num1 == 0:
         return 1
-    return fibonacci(num1-1) + fibonacci(num1-2)
+    return recur_fibonacci(num1-1) + recur_fibonacci(num1-2)
+
+def iter_fibonacci(n):
+    # fib of a term is the addition of the previous two terms in the sequence
+    if n == 0:
+        return 1
+    a = 0 # first no. of fib seq
+    b = 1 # second no. of fib seq
+    # [a,b,ans] structure
+    for _ in range(1,n+1): # loop for "n" number of terms. Offset to start from 1
+        ans = a + b # adds the previous 2 terms
+        a = b
+        b = ans
+    return ans # return the final ans
 
 if __name__ == "__main__":
-    print(fibonacci(int(input("Enter a number for the fibonacci:"))))
+    print(recur_fibonacci(int(input("Enter a number for the fibonacci:"))))
+    print(iter_fibonacci(int(input("Enter the term to find the fibonacci number of that term:"))))
