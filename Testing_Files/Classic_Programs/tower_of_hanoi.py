ca208803997d60c0f91170cfafcee9e584606a4a
Abheek Satishkumar <147516145+abheekskumar@users.noreply.github.com>
2025-06-22 14:27:17 -0700

    Added Files

--- Testing_Files/Classic_Programs/tower_of_hanoi.py
+++ Testing_Files/Classic_Programs/tower_of_hanoi.py
@@ -6,6 +6,15 @@ The property the plates have is as follows: no plate can sit atop one smaller th
 
 The problem states is to give a set of instructions to the "priests" of Hanoi to move the disks from 
 one tower to the other. 
+
+Thinking about the problem in general:
+think about move all but one disk to the spare, move the last disk to the desired tower. and then repeat 
+this until all the disks are put onto the desired tower.
+Base case: when you're at the last disk in the spare, put it onto the desired tower
+Recursive case: move all but one disk to a spare tower; move the one disk to the desired tower, repeat
+
+How to implement it one by one:
+Move the top disk to the spare, move the 
 """
 def print_move(frm,to):
     """
@@ -13,10 +22,14 @@ def print_move(frm,to):
     """
     print("Move disk from "+str(frm)+" to "+str(to))
 
-def tower(n,):
-    # How to think of this problem: 
-    # Move 
-    # base case: when it's the last tower(biggest), print out the command
+def towers(n,frm,to,spr):
+    # Base case: when the spare only has one disk, move it into the desired position.
+    if n == 1:
+        print_move(frm,to)
+
+    towers()
+    # Recursive case: Move all but one disk to the spare; Move 1 disk(the largest) to the desired tower
 
 
-    # recursive case: Otherwise, move  
+if __name__ == "__main__":
+    towers(4,"Tower_1","Tower_2","Tower_3")
