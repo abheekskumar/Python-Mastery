6234699e8ce61aee73d2b45f2974e1473b6ba286
Abheek Satishkumar <147516145+abheekskumar@users.noreply.github.com>
2025-07-15 11:12:40 -0700

    Started plotting

--- CSE_1_Coursework/6_algorithmic_complexity/mergeSort.py
+++ CSE_1_Coursework/6_algorithmic_complexity/mergeSort.py
@@ -55,8 +55,22 @@ def mergeSort(L):
             return mergeSortMerger(L1,L2)
     return mergeSortRecur(L)
 
+
+def modSwapSort(L): 
+    """ L is a list on integers """
+    print("Original L: ", L)
+    for i in range(len(L)):
+        for j in range(len(L)):
+            if L[j] < L[i]:
+                # the next line is a short 
+                # form for swap L[i] and L[j]
+                L[j], L[i] = L[i], L[j] 
+                print(L)
+    print("Final L: ", L)
+    
 L = [636,23,523,975,33,62,34,626,63]
 print(mergeSort(L))
+print(modSwapSort(L))
     
     
     
---
+++ CSE_1_Coursework/7_plotting/plotting.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+mySamples = []
+myLinear = []
+myQuadratic = []
+myCubic = []
+myExponential = []
+
+for i in range(0,30):
+    mySamples.append(i)
+    myLinear.append(i)
+    myQuadratic.append(i**2)
+    myCubic.append(i**3)
+    myExponential(2**i)
+
+plt.plot()
