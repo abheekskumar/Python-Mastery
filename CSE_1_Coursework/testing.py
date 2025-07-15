08791d3f87e6ba54ec4026715c24208f75434305
Abheek Satishkumar <147516145+abheekskumar@users.noreply.github.com>
2025-07-15 09:35:51 -0700

    Finshed algorithmic complexity

--- CSE_1_Coursework/testing.py
+++ CSE_1_Coursework/testing.py
@@ -1,36 +1,37 @@
-stringa = str()
-print(stringa)
+# stringa = str()
+# print(stringa)
 
-stringb = stringa + "d"
-stringb += stringa
-stringb += "d"
-print(stringb)
+# stringb = stringa + "d"
+# stringb += stringa
+# stringb += "d"
+# print(stringb)
 
-x = {0: 2, 1: 1, 2: 0, 3: 0, 8: 0}
-s1 = "Hello World"
-l1 = s1.split()
-print(l1.reverse())
+# x = {0: 2, 1: 1, 2: 0, 3: 0, 8: 0}
+# s1 = "Hello World"
+# l1 = s1.split()
+# print(l1.reverse())
 
-def memo_fib(n,d= {1:1,2:1}):
-    """
-    Fibonacci number using memoization with a default dictionary
-    """
-    if n in d: # if it is in the dict
-        return d[n]
-    else: # if it isn't in the dict
-        ans = memo_fib(n-1,d) + memo_fib(n-2,d) # recursively calculate it
-        d[n] = ans # add to dict
-        return ans # return to above recursive call
+# def memo_fib(n,d= {1:1,2:1}):
+#     """
+#     Fibonacci number using memoization with a default dictionary
+#     """
+#     if n in d: # if it is in the dict
+#         return d[n]
+#     else: # if it isn't in the dict
+#         ans = memo_fib(n-1,d) + memo_fib(n-2,d) # recursively calculate it
+#         d[n] = ans # add to dict
+#         return ans # return to above recursive call
     
-dict1 = {'a':2,'b':3,"c":0}
+# dict1 = {'a':2,'b':3,"c":0}
 
-for letter in dict1:
-    print(letter)
-for letter in dict1.keys():
-    print(letter)
-for letter in dict1.keys():
-    if dict1[letter]==0:
-        print(letter)
-        print(dict1[letter])
-        del dict1[letter] 
-        print(dict1)
+# for letter in dict1:
+#     print(letter)
+# for letter in dict1.keys():
+#     print(letter)
+# for letter in dict1.keys():
+#     if dict1[letter]==0:
+#         print(letter)
+#         print(dict1[letter])
+#         del dict1[letter] 
+#         print(dict1)
+
