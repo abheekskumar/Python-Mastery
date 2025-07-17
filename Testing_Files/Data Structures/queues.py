278ff2511e7db5b9d3b9fcb42744d616255dbcdf
Abheek Satishkumar <147516145+abheekskumar@users.noreply.github.com>
2025-07-17 08:46:40 -0700

    Added files

---
+++ Testing_Files/Data Structures/queues.py
@@ -0,0 +1,56 @@
+# OOP approach:
+
+class Queue():
+    def __init__(self):
+        """Initializes an empty queue based on FIFO principle."""
+        self.queue = []
+    def __str__(self):
+        return str(self.queue)
+    def enqueue(self,element):
+        self.queue.append(element)
+    def dequeue(self):
+        try:
+            return self.queue.pop(0)
+        except Exception:
+            return "Queue is empty."
+    def front(self):
+        try:
+            return self.queue[0]
+        except Exception:
+            return "Queue is empty"
+q1 = Queue()
+q1.enqueue(23)
+q1.enqueue(65)
+q1.enqueue(87)
+q1.enqueue(54)
+print(q1.dequeue())
+print("*")
+print(q1)
+
+# Functional approach:
+
+def queueEnqueue(queue: list,element)->None:
+    """ Enqueues element onto the back of the queue."""
+    queue.append(element)
+def queueDequeue(queue:list):
+    """ Dequeues frontmost element of the queue."""
+    try:
+        return queue.pop(0)
+    except Exception:
+        return "Queue is empty"
+def queueFront(queue:list):
+    """ Returns the first element in the queue."""
+    try:
+        return queue[0]
+    except Exception:
+        return "Queue is empty"
+    
+q2 = []
+queueEnqueue(q2,23)
+queueEnqueue(q2,65)
+queueEnqueue(q2,87)
+queueEnqueue(q2,54)
+print("Functional Approach:")
+print(queueDequeue(q2))
+print("*")
+print(q2)
---
+++ Testing_Files/Data Structures/stacks.py
@@ -0,0 +1,71 @@
+# OOP Approach:
+class Stack:
+    def __init__(self):
+        """ Initializes an empty stack based on LIFO principle."""
+        self.stack = []
+    def __str__(self):
+        res = ""
+        for idx in range(len(self.stack)-1,-1,-1):
+            res += str(self.stack[idx])
+            res += "\n"
+        return res
+
+    def push(self,element) -> None:
+        """ Pushes element onto the top of the stack"""
+        self.stack.append(element)
+    def pop(self):
+        """ Removes and returns the element on the top of the stack."""
+        try:
+            return self.stack.pop()
+        except Exception:
+            return "Stack is empty."
+    def peak(self):
+        """Returns the topmost element of the stack."""
+        try:
+            return self.stack[-1]
+        except:
+            return "Stack is empty."
+    
+
+s1 = Stack()
+s1.push(1)
+s1.push(2)
+s1.push(3)
+s1.push(4)
+s1.push(5)
+print(s1)
+s1.pop()
+print(s1)
+
+# Functional approach:
+
+def pushStack(stack:list,element)-> None:
+    """Pushes element onto the top of the stack"""
+    stack.append(element)
+
+def peakStack(stack:list):
+    """Returns topmost element of the stack. If empty, returns a distinct statement."""
+    try:
+        return stack[-1]
+    except Exception:
+        return "Stack is empty."
+    
+def popStack(stack:list):
+    """ Removes and returns the topmost element of the stack. If empty, returns a distinct statement."""
+    try:
+        return stack.pop()
+    except Exception:
+        return "Unable to pop as stack is empty."
+
+s2 = []
+pushStack(s2,1)
+pushStack(s2,2)
+pushStack(s2,3)
+pushStack(s2,4)
+pushStack(s2,5)
+print(s2)
+popStack(s2)
+print(s2)
+
+
+    
